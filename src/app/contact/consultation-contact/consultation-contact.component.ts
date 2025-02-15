5319ee87473f5a0c40ef277f5c7619a8cd38b5f0
Victor Oliveira <vitinifal@gmail.com>
2025-02-15 01:12:15 -0300

    fix: editar

--- src/app/contact/consultation-contact/consultation-contact.component.ts
+++ src/app/contact/consultation-contact/consultation-contact.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Contact } from '../../interfaces/contact';
 import { ContactService } from '../../services/contact.service';
 import { CommonModule, KeyValuePipe } from '@angular/common';
 import { FavoriteFilterPipe } from '../../pipes/favorite-filter.pipe';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActiveFilterPipe } from '../../active-filter.pipe';
 
 @Component({
   selector: 'app-consultation-contact',
-  imports: [CommonModule, FavoriteFilterPipe, ActiveFilterPipe, FormsModule],
+  imports: [
+    CommonModule,
+    FavoriteFilterPipe,
+    ActiveFilterPipe,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [KeyValuePipe],
 
   templateUrl: './consultation-contact.component.html',
@@ -19,16 +31,56 @@ export class ConsultationContactComponent implements OnInit {
   favoriteContacts: Contact[] = [];
   showFavorites: boolean = false;
   showActive: boolean = true;
-
-  constructor(private contactSvc: ContactService) {}
+  editMode: boolean = false;
+  editIndex: number = -1;
+  formEdit!: FormGroup;
+  constructor(
+    private contactSvc: ContactService,
+    private fb: FormBuilder,
+    private cdRef: ChangeDetectorRef
+  ) {}
   ngOnInit(): void {
     this.contacts = this.contactSvc.getContactsFromLocalStorage();
     console.log(this.contacts, 'VICTOR');
     this.favoriteContacts = this.contacts.filter((contact) => contact.favorite);
     console.log(this.favoriteContacts, 'VICTOR');
+
+    this.formEdit = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.maxLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
+      favorite: [false],
+      active: [true],
+    });
   }
 
-  editContact(contact: Contact) {}
+  editContact(index: number): void {
+    const contact = this.contacts[index];
+    this.editIndex = index;
+    this.formEdit.setValue({
+      name: contact.name,
+      phone: contact.phone,
+      email: contact.email,
+      favorite: contact.favorite,
+      active: contact.active,
+    });
+    this.editMode = true; // Ativa o modo de edição
+  }
 
-  deleteContact(contact: Contact) {}
+  editContactSubmit(): void {
+    if (this.formEdit.valid) {
+      const updatedContact = this.formEdit.value;
+      this.contacts[this.editIndex] = {
+        ...this.contacts[this.editIndex],
+        ...updatedContact,
+      };
+      this.formEdit.reset();
+      this.editMode = false; // Desativa o modo de edição após salvar
+      this.cdRef.detectChanges();
+    }
+  }
+
+  deleteContact(index: number): void {
+    this.contacts.splice(index, 1);
+  }
 }
